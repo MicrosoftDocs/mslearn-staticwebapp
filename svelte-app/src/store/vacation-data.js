f4ace0b7fdf471d4b5429387ea690d4eb5a84522
John Papa <john@johnpapa.net>
2020-04-15 15:09:41 -0400

    removed axios fro svelte because svelte mindset would be to use fetch

--- svelte-app/src/store/vacation-data.js
+++ svelte-app/src/store/vacation-data.js
@@ -1,12 +1,13 @@
-import axios from 'axios';
 import * as store from './store';
 import { parseItem, parseList } from './http-utils';
 import API from './config';
 
 export async function getVacationsAction() {
   try {
-    const response = await axios.get(`${API}/vacations`);
-    const vacations = parseList(response);
+    const response = await fetch(`${API}/vacations`, {
+      method: 'GET',
+    });
+    const vacations = await parseList(response);
     store.getVacations(vacations);
     return vacations;
   } catch (error) {
@@ -16,8 +17,10 @@ export async function getVacationsAction() {
 
 export async function deleteVacationAction(vacation) {
   try {
-    const response = await axios.delete(`${API}/vacations/${vacation.id}`);
-    parseItem(response, 200);
+    const response = await fetch(`${API}/vacations/${vacation.id}`, {
+      method: 'DELETE',
+    });
+    await parseItem(response, 200);
     store.deleteVacation(vacation);
     return null;
   } catch (error) {
@@ -26,11 +29,14 @@ export async function deleteVacationAction(vacation) {
 }
 export async function updateVacationAction(vacation) {
   try {
-    const response = await axios.put(
-      `${API}/vacations/${vacation.id}`,
-      vacation
-    );
-    const updatedVacation = parseItem(response, 200);
+    const response = await fetch(`${API}/vacations/${vacation.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(vacation),
+    });
+    const updatedVacation = await parseItem(response, 200);
     store.updateVacation(updatedVacation);
     return updatedVacation;
   } catch (error) {
@@ -39,8 +45,14 @@ export async function updateVacationAction(vacation) {
 }
 export async function addVacationAction(vacation) {
   try {
-    const response = await axios.post(`${API}/vacations`, vacation);
-    const addedVacation = parseItem(response, 201);
+    const response = await fetch(`${API}/vacations`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(vacation),
+    });
+    const addedVacation = await parseItem(response, 201);
     store.addVacation(addedVacation);
     return addedVacation;
   } catch (error) {
