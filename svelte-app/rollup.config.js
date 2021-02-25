4b387566d34f7061f1207dbdffa8447370299ae3
sinedied <noda@free.fr>
2021-02-25 11:14:21 +0100

    Update Angular, React, Vue and Svelte apps

--- svelte-app/rollup.config.js
+++ svelte-app/rollup.config.js
@@ -27,15 +27,18 @@ export default {
           SVELTE_APP_API: API,
         },
       }),
+      preventAssignment: true
     }),
     svelte({
-      // enable run-time checks when not in production
-      dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file - better for performance
-      css: (css) => {
-        css.write('public/build/bundle.css');
-      },
+      compilerOptions: {
+        // enable run-time checks when not in production
+        dev: !production,
+        // we'll extract any component CSS out into
+        // a separate file - better for performance
+        css: (css) => {
+          css.write('public/build/bundle.css');
+        },
+      }
     }),
     postcss(),
 
