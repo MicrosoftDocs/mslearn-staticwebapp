4c276b72799eadb92b79a7390e5d92486da0498e
Sara Gibbons <saragibby@gmail.com>
2023-06-16 19:47:12 -0400

    update react app packages

---
+++ react-app/eslint.config.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    "extends": [
+      "eslint:recommended",
+      "plugin:import/errors",
+      "plugin:react/recommended",
+      "plugin:jsx-a11y/recommended",
+      "prettier",
+      "airbnb"
+    ],
+    "rules": {
+      "react/prop-types": 0,
+      "jsx-a11y/label-has-for": 0,
+      "jsx-a11y/anchor-is-valid": 0,
+      "jsx-a11y/click-events-have-key-events": 0,
+      "no-console": 1,
+      "quotes": [
+        2,
+        "single",
+        {
+          "avoidEscape": true,
+          "allowTemplateLiterals": true
+        }
+      ]
+    },
+    "plugins": ["react", "import", "jsx-a11y"],
+    "parser": "react-scripts/node_modules/babel-eslint",
+    "parserOptions": {
+      "ecmaVersion": 2018,
+      "sourceType": "module",
+      "ecmaFeatures": {
+        "jsx": true
+      }
+    },
+    "env": {
+      "es6": true,
+      "browser": true,
+      "node": true
+    }
+  }
+
+]
